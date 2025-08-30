e53364e3de7b4b2c4549b5dd30eb8851da297e1e
4shm1td06 <alpha5sector@gmail.com>
2025-08-30 11:40:41 +0530

    Update NewsEvents.js

--- src/components/news-events/NewsEvents.js
+++ src/components/news-events/NewsEvents.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import NewsList from './NewsList';
-import EventsList from './EventsList';
+
 
 const NewsEvents = () => {
   return (
     <div className="p-6 space-y-6">
       <h1 className="text-2xl font-bold">News & Events</h1>
       <NewsList />
-      <EventsList />
     </div>
   );
 };
